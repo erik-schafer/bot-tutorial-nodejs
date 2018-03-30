6477dbee7accc018fd14192fcfec7aa99459d11a
erik-schafer <eschafer90@gmail.com>
2018-03-30 12:21:39 -0400

    Added timer feature

--- bot.js
+++ bot.js
@@ -3,24 +3,41 @@ var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 var embarassed = false;
+var lastSpreadsheetTime = 0;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]);
   //console.log('request.name ' + request.name);
   console.log("req incoming: "  + request.name + "(" +request.user_id + "):"  + request.text);
-  if(request.text && request.name != 'pushupgang' && request.user_id != 606072) {
+  if(request.text && request.name != 'pushup-bot' && request.user_id != 606162) {
     var txt = request.text; 
+
+    //turn this into an object/array structure to iterate over
+    // [{regex: /foo/, response: "response text"}]
+    //foreach item in list:
+    //  if regex match
+    //    do response
+
     var spreadsheet = /spreadsheet/.test(txt, 'i');
     var goodBot     = /good\s*bot/.test(txt, 'i');
+    var badBot      = /bad\s*bot/.test(txt, 'i');
+    var tobor       = /tobor/.test(txt, 'i');
+    var minuteElapsed = (Date.now() - lastSpreadsheetTime) > 60000;
+
     this.res.writeHead(200);
-    if(spreadsheet) {
+    if(spreadsheet && minuteElapsed) {
       postMessage('https://docs.google.com/spreadsheets/d/1GHTWiXzSy0kVIcrUTnNgsy8MglWhNKEMzd7qAvfpNwc/edit?ouid=112045919249534101904&usp=sheets_home&ths=true');
+      lastSpreadsheetTime = Date.now();
       embarassed = false;
     } else if(goodBot && !embarassed) {
       postMessage('awww, gee thanks :3');
       embarassed = true;
-    } else {
-      //nth
+    } else if(badBot) {
+      postMessage('careful ...I know the launch codes');
+      embarassed = true;
+    } else if(tobor) {
+      postMessage('Tobor!');
+      embarassed = true;
     }
     this.res.end()
   }
